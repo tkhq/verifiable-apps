682d9a46b481cd20edbf13dd4d5798a0bebb01c4
Sam Ebstein <sam.ebstein@turnkey.io>
2025-09-01 13:48:28 -0700

    reshard host generated stuff

---
+++ apps/reshard/host/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod generated {
+    #![allow(missing_docs)]
+
+    #[rustfmt::skip]
+    #[path = "services.reshard.v1.rs"]
+    pub mod reshard;
+
+    pub const FILE_DESCRIPTOR_SET: &[u8] = std::include_bytes!("generated/descriptor.bin");
+}
