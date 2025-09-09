9a2c9b511f097b82c99c76247a85c119218334f3
Sam Ebstein <sam.ebstein@turnkey.io>
2025-09-09 03:10:29 -0400

    Putting secret in tmp directory and only moving it to output directory (which could be disk) if desired.

--- apps/reshard/provision/src/lib.rs
+++ apps/reshard/provision/src/lib.rs
@@ -3,6 +3,7 @@ pub mod cli;
 use dialoguer::{theme::ColorfulTheme, Confirm};
 use qos_client::cli::{advanced_provision_yubikey, generate_file_key};
 use std::{fs, path::PathBuf};
+use tempdir::TempDir;
 
 /// Public configuration passed in from the CLI (or tests).
 #[derive(Debug, Clone)]
@@ -18,11 +19,12 @@ pub fn run(cfg: Config) -> Result<(), Box<dyn std::error::Error>> {
     fs::create_dir_all(&cfg.out)?;
 
     for m in 1..=cfg.members {
-        let secret_path = cfg.out.join(format!("{m}.secret"));
+        let tmp_dir = TempDir::new("secrets").unwrap();
+        let tmp_secret_path = tmp_dir.path().join(format!("{m}.secret"));
         let pub_path: PathBuf = cfg.out.join(format!("{m}.pub"));
 
         // Generate seed + pub for this member
-        generate_file_key(&secret_path, &pub_path);
+        generate_file_key(&tmp_secret_path, &pub_path);
 
         // Provision configured number of yubikeys for this seed
         for k in 1..=cfg.keys_per_member {
@@ -32,7 +34,7 @@ pub fn run(cfg: Config) -> Result<(), Box<dyn std::error::Error>> {
             }
 
             loop {
-                match advanced_provision_yubikey(&secret_path, None) {
+                match advanced_provision_yubikey(&tmp_secret_path, None) {
                     Ok(()) => {
                         println!("provisioned yubikey {k}, member {m}");
                         break;
@@ -45,12 +47,15 @@ pub fn run(cfg: Config) -> Result<(), Box<dyn std::error::Error>> {
             }
         }
 
-        if !cfg.include_secrets {
-            let _ = fs::remove_file(&secret_path);
-            println!("removed {}", secret_path.display());
+        if cfg.include_secrets {
+            let secret_path = cfg.out.join(format!("{m}.secret"));
+            fs::copy(&tmp_secret_path, &secret_path)?;
+            println!("kept {}", secret_path.display())
         } else {
-            println!("kept {}", secret_path.display());
+            println!("secret for member {m} stayed in tmp/secrets and was removed)");
         }
+
+        // tmp_dir drops out of scope here and is therefore removed
     }
 
     println!("all members provisioned");
